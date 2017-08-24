8a7af811672e6f383ab205c5c60b54566a5ef2ec
Justus <justusbrown13@gmail.comgit config --global user.email justusbrown13@gmail.com>
2017-08-23 21:03:25 -0500

    idk if this is right? i added a conversion to mrst

---
+++ mrst-2017a/utils/units/lbm_ft3.m
@@ -0,0 +1,8 @@
+function rho = lbm_ft3()
+%lbm/ft^3 Summary of this function goes here
+%   Detailed explanation goes here
+
+%Converts from the units of lbm/ft^3 to kg/m^3
+rho=16.0185*kilo()*gram()/(meter()^3)
+end
+
